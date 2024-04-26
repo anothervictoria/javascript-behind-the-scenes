86da4b2db9c63815a410a292aab2fe17914b5dd4
anothervictoria <victoria.panidi@gmail.com>
2024-04-26 18:12:29 +0400

    this keyword in practice

--- script.js
+++ script.js
@@ -1,36 +1,107 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
 
-      // Creating NEW variable with same name as outer score's variable
-      const firstName = 'Steven';
+//       // Creating NEW variable with same name as outer score's variable
+//       const firstName = 'Steven';
 
-      //Reassigning outer scope's variable
-      output = `NEW OUTPUT`;
+//       //Reassigning outer scope's variable
+//       output = `NEW OUTPUT`;
 
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
 
-    // console.log(add(2, 3)); //gives an error only in strict mode
-    console.log(millenial);
-    console.log(output);
-  }
-  printAge();
+//     // console.log(add(2, 3)); //gives an error only in strict mode
+//     console.log(millenial);
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Victoria';
+// calcAge(1991);
+
+//Hoisting with variables
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// //Hoisting functions
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow);
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+//Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
 }
 
-const firstName = 'Victoria';
+var x = 1;
+let y = 2;
+const z = 3;
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
 calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+// console.log(matilda.calcAge);
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
