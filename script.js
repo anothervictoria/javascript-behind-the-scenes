dbea322fe978326206166eefa96cf6313f059352
anothervictoria <victoria.panidi@gmail.com>
2024-04-26 19:03:05 +0400

    primitive vs reference types

--- script.js
+++ script.js
@@ -106,53 +106,69 @@
 // const f = jonas.calcAge;
 // f();
 
-var firstName = 'Matilda';
-const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    // console.log(this);
-    console.log(2037 - this.year);
+// var firstName = 'Matilda';
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     // console.log(this);
+//     console.log(2037 - this.year);
 
-    //Solution 1
-    // const self = this; //self or that
-    // const isMillenial = function () {
-    //   console.log(this);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    //   // console.log(this.year >= 1981 && this.year <= 1996);
-    // };
+//     //Solution 1
+//     // const self = this; //self or that
+//     // const isMillenial = function () {
+//     //   console.log(this);
+//     //   console.log(self.year >= 1981 && self.year <= 1996);
+//     //   // console.log(this.year >= 1981 && this.year <= 1996);
+//     // };
 
-    // Solution 2
+//     // Solution 2
 
-    //works because arrow function uses this keyword from it's parent's scope
+//     //works because arrow function uses this keyword from it's parent's scope
 
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-    };
-    isMillenial();
-  },
+//     const isMillenial = () => {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+//     isMillenial();
+//   },
 
-  greet: () => {
-    console.log(this);
-    console.log(`Hey, ${this.firstName}`);
-  },
+//   greet: () => {
+//     console.log(this);
+//     console.log(`Hey, ${this.firstName}`);
+//   },
+// };
+
+// jonas.greet();
+// // console.log(this.firstName);
+// jonas.calcAge();
+
+// // Arguments keyword
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addExpr(2, 5);
+// addExpr(2, 5, 6, 7, 8);
+
+// var addArrow = (a, b) => {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addArrow(8, 9, 10);
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
 };
 
-jonas.greet();
-// console.log(this.firstName);
-jonas.calcAge();
-
-// Arguments keyword
-const addExpr = function (a, b) {
-  console.log(arguments);
-  return a + b;
-};
-addExpr(2, 5);
-addExpr(2, 5, 6, 7, 8);
-
-var addArrow = (a, b) => {
-  console.log(arguments);
-  return a + b;
-};
-addArrow(8, 9, 10);
+const friend = me;
+friend.age = 27;
+console.log(`Friend:`, friend);
+console.log(`Me:`, me);
